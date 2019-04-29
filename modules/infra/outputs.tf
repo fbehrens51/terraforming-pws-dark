e0d0539d7b34912186468261ab7c36d2861bc649
Jared Gordon <jgordon@pivotal.io>
2019-04-29 11:59:43 -0400

    remove endpoint logic. (#13)

--- modules/infra/outputs.tf
+++ modules/infra/outputs.tf
@@ -49,15 +49,3 @@ output "infrastructure_subnet_cidrs" {
 output "infrastructure_subnet_gateways" {
   value = ["${data.template_file.infrastructure_subnet_gateways.*.rendered}"]
 }
-
-output "aws_lb_interface_endpoint_ips" {
-  value = "${data.aws_network_interface.lb_endpoints.*.private_ip}"
-}
-
-output "aws_ec2_interface_endpoint_ips" {
-  value = "${data.aws_network_interface.ec2_endpoints.*.private_ip}"
-}
-
-output "region" {
-  value = "${data.aws_region.current.name}"
-}
