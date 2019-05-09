2affe8323bd2211b60aff9bb599510d672a59d7e
Patrick Oyarzun <poyarzun@pivotal.io>
2019-05-09 19:50:35 +0000

    dark_providers: Pin the version of the AWS provider
    Also relax the other versions, so that any patch version is accepted.
    
    [#165384961](https://www.pivotaltracker.com/story/show/165384961)
    
    Co-authored-by: Patrick Oyarzun <poyarzun@pivotal.io>
    Co-authored-by: John Shahid <jshahid@pivotal.io>

--- modules/dark_providers/providers.tf
+++ modules/dark_providers/providers.tf
@@ -1,13 +1,17 @@
+provider "aws" {
+  version = "~> 1.54.0"
+}
+
 provider "random" {
-  version = "1.3.1"
+  version = "~> 1.3.0"
 }
 
 provider "tls" {
-  version = "1.2.0"
+  version = "~> 1.2.0"
 }
 
 provider "template" {
-  version = "2.0.0"
+  version = "~> 2.0.0"
 }
 
 provider "null" {}
