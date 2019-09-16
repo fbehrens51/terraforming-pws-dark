3ff27d8a31abde4c4eeed5afe6d1ca186e5bdcc9
Patrick Oyarzun <poyarzun@pivotal.io>
2019-09-16 22:06:53 +0000

    Add a route from PAS Public to ES
    - Also refactor the routing layer to more clearly add routes to / from all vpcs.
    - Applying this change will re-associate all subnets with the vpc default route
      table, which effectively breaks internet communication for every vm in the
      system.
    - Re-applying the layers which associate subnets with route tables is enough to
      fix it.
    
    [#168480627](https://www.pivotaltracker.com/story/show/168480627)
    
    Co-authored-by: Patrick Oyarzun <poyarzun@pivotal.io>
    Co-authored-by: John Shahid <jshahid@pivotal.io>

--- layers/routes/main.tf
+++ layers/routes/main.tf
@@ -29,168 +29,40 @@ module "vpc_route_tables" {
   internetless   = "${var.internetless}"
 }
 
-data "aws_vpc" "bastion_vpc" {
-  id = "${data.terraform_remote_state.paperwork.bastion_vpc_id}"
+module "route_bastion_cp" {
+  source                   = "../../modules/routing"
+  accepter_vpc_id  = "${data.terraform_remote_state.paperwork.bastion_vpc_id}"
+  requester_vpc_id = "${data.terraform_remote_state.paperwork.cp_vpc_id}"
 }
 
-data "aws_vpc" "pas_vpc" {
-  id = "${data.terraform_remote_state.paperwork.pas_vpc_id}"
-}
-
-data "aws_vpc" "cp_vpc" {
-  id = "${data.terraform_remote_state.paperwork.cp_vpc_id}"
-}
-
-data "aws_vpc" "es_vpc" {
-  id = "${data.terraform_remote_state.paperwork.es_vpc_id}"
-}
-
-data "aws_vpc_peering_connection" "pas_bastion_peering_connection" {
-  vpc_id      = "${data.terraform_remote_state.paperwork.pas_vpc_id}"
-  peer_vpc_id = "${data.terraform_remote_state.paperwork.bastion_vpc_id}"
-}
-
-resource "aws_route" "pas_private_to_bastion" {
-  route_table_id            = "${module.vpc_route_tables.pas_private_vpc_route_table_id}"
-  destination_cidr_block    = "${data.aws_vpc.bastion_vpc.cidr_block}"
-  vpc_peering_connection_id = "${data.aws_vpc_peering_connection.pas_bastion_peering_connection.id}"
-
-  timeouts {
-    create = "5m"
-  }
-}
-
-data "aws_vpc_peering_connection" "es_bastion_peering_connection" {
-  vpc_id      = "${data.terraform_remote_state.paperwork.es_vpc_id}"
-  peer_vpc_id = "${data.terraform_remote_state.paperwork.bastion_vpc_id}"
-}
-
-data "aws_vpc_peering_connection" "es_pas_peering_connection" {
-  vpc_id      = "${data.terraform_remote_state.paperwork.es_vpc_id}"
-  peer_vpc_id = "${data.terraform_remote_state.paperwork.pas_vpc_id}"
-}
-
-data "aws_vpc_peering_connection" "pas_cp_peering_connection" {
-  vpc_id      = "${data.terraform_remote_state.paperwork.pas_vpc_id}"
-  peer_vpc_id = "${data.terraform_remote_state.paperwork.cp_vpc_id}"
-}
-
-data "aws_vpc_peering_connection" "cp_bastion_peering_connection" {
-  vpc_id      = "${data.terraform_remote_state.paperwork.cp_vpc_id}"
-  peer_vpc_id = "${data.terraform_remote_state.paperwork.bastion_vpc_id}"
-}
-
-data "aws_vpc_peering_connection" "es_cp_peering_connection" {
-  vpc_id      = "${data.terraform_remote_state.paperwork.es_vpc_id}"
-  peer_vpc_id = "${data.terraform_remote_state.paperwork.cp_vpc_id}"
-}
-
-resource "aws_route" "cp_public_to_es" {
-  route_table_id            = "${module.vpc_route_tables.cp_public_vpc_route_table_id}"
-  destination_cidr_block    = "${data.aws_vpc.es_vpc.cidr_block}"
-  vpc_peering_connection_id = "${data.aws_vpc_peering_connection.es_cp_peering_connection.id}"
-
-  timeouts {
-    create = "5m"
-  }
-}
-
-resource "aws_route" "cp_private_to_bastion" {
-  route_table_id            = "${module.vpc_route_tables.cp_private_vpc_route_table_id}"
-  destination_cidr_block    = "${data.aws_vpc.bastion_vpc.cidr_block}"
-  vpc_peering_connection_id = "${data.aws_vpc_peering_connection.cp_bastion_peering_connection.id}"
-
-  timeouts {
-    create = "5m"
-  }
-}
-
-resource "aws_route" "cp_private_to_pas" {
-  route_table_id            = "${module.vpc_route_tables.cp_private_vpc_route_table_id}"
-  destination_cidr_block    = "${data.aws_vpc.pas_vpc.cidr_block}"
-  vpc_peering_connection_id = "${data.aws_vpc_peering_connection.pas_cp_peering_connection.id}"
-
-  timeouts {
-    create = "5m"
-  }
-}
-
-resource "aws_route" "es_private_to_bastion" {
-  route_table_id            = "${module.vpc_route_tables.es_private_vpc_route_table_id}"
-  destination_cidr_block    = "${data.aws_vpc.bastion_vpc.cidr_block}"
-  vpc_peering_connection_id = "${data.aws_vpc_peering_connection.es_bastion_peering_connection.id}"
-
-  timeouts {
-    create = "5m"
-  }
-}
-
-resource "aws_route" "es_private_to_pas" {
-  route_table_id            = "${module.vpc_route_tables.es_private_vpc_route_table_id}"
-  destination_cidr_block    = "${data.aws_vpc.pas_vpc.cidr_block}"
-  vpc_peering_connection_id = "${data.aws_vpc_peering_connection.es_pas_peering_connection.id}"
-
-  timeouts {
-    create = "5m"
-  }
-}
-
-resource "aws_route" "pas_public_to_cp" {
-  route_table_id            = "${module.vpc_route_tables.pas_public_vpc_route_table_id}"
-  destination_cidr_block    = "${data.aws_vpc.cp_vpc.cidr_block}"
-  vpc_peering_connection_id = "${data.aws_vpc_peering_connection.pas_cp_peering_connection.id}"
-
-  timeouts {
-    create = "5m"
-  }
-}
-
-resource "aws_route" "es_public_to_cp" {
-  route_table_id            = "${module.vpc_route_tables.es_public_vpc_route_table_id}"
-  destination_cidr_block    = "${data.aws_vpc.cp_vpc.cidr_block}"
-  vpc_peering_connection_id = "${data.aws_vpc_peering_connection.es_cp_peering_connection.id}"
-
-  timeouts {
-    create = "5m"
-  }
-}
-
-// We can't know in general which vpc is the accepter vs the requester,
-// so these modules have to be copied in each environment
 module "route_bastion_pas" {
   source                   = "../../modules/routing"
-  accepter_route_table_id  = "${module.vpc_route_tables.bastion_public_vpc_route_table_id}"
-  requester_route_table_id = "${module.vpc_route_tables.pas_public_vpc_route_table_id}"
+  accepter_vpc_id  = "${data.terraform_remote_state.paperwork.bastion_vpc_id}"
+  requester_vpc_id = "${data.terraform_remote_state.paperwork.pas_vpc_id}"
 }
 
-module "route_bastion_control_plane" {
+module "route_bastion_es" {
   source                   = "../../modules/routing"
-  accepter_route_table_id  = "${module.vpc_route_tables.bastion_public_vpc_route_table_id}"
-  requester_route_table_id = "${module.vpc_route_tables.cp_public_vpc_route_table_id}"
+  accepter_vpc_id  = "${data.terraform_remote_state.paperwork.bastion_vpc_id}"
+  requester_vpc_id = "${data.terraform_remote_state.paperwork.es_vpc_id}"
 }
 
-module "route_bastion_enterprise_services" {
+module "route_cp_pas" {
   source                   = "../../modules/routing"
-  accepter_route_table_id  = "${module.vpc_route_tables.bastion_public_vpc_route_table_id}"
-  requester_route_table_id = "${module.vpc_route_tables.es_public_vpc_route_table_id}"
+  accepter_vpc_id  = "${data.terraform_remote_state.paperwork.cp_vpc_id}"
+  requester_vpc_id = "${data.terraform_remote_state.paperwork.pas_vpc_id}"
 }
 
-module "route_pas_private_es" {
+module "route_cp_es" {
   source                   = "../../modules/routing"
-  accepter_route_table_id  = "${module.vpc_route_tables.pas_private_vpc_route_table_id}"
-  requester_route_table_id = "${module.vpc_route_tables.es_public_vpc_route_table_id}"
+  accepter_vpc_id  = "${data.terraform_remote_state.paperwork.cp_vpc_id}"
+  requester_vpc_id = "${data.terraform_remote_state.paperwork.es_vpc_id}"
 }
 
-module "route_cp_pas_private" {
+module "route_pas_es" {
   source                   = "../../modules/routing"
-  accepter_route_table_id  = "${module.vpc_route_tables.cp_public_vpc_route_table_id}"
-  requester_route_table_id = "${module.vpc_route_tables.pas_private_vpc_route_table_id}"
-}
-
-module "route_cp_private_es" {
-  source                   = "../../modules/routing"
-  accepter_route_table_id  = "${module.vpc_route_tables.cp_private_vpc_route_table_id}"
-  requester_route_table_id = "${module.vpc_route_tables.es_private_vpc_route_table_id}"
+  accepter_vpc_id  = "${data.terraform_remote_state.paperwork.pas_vpc_id}"
+  requester_vpc_id = "${data.terraform_remote_state.paperwork.es_vpc_id}"
 }
 
 variable "remote_state_region" {}
--- modules/routing/vpc_route_tables/main.tf
+++ modules/routing/vpc_route_tables/main.tf
@@ -5,100 +5,74 @@ variable "cp_vpc_id" {}
 variable "env_name" {}
 variable "internetless" {}
 
-resource "aws_route_table" "pas_private_route_table" {
-  count = 1
-
-  vpc_id = "${var.pas_vpc_id}"
-
-  tags {
-    Name = "${var.env_name} | PAS PRIVATE"
-  }
-}
-
-resource "aws_route_table" "cp_private_route_table" {
-  count = 1
-
-  vpc_id = "${var.cp_vpc_id}"
-
-  tags {
-    Name = "${var.env_name} | CP PRIVATE"
-  }
-}
-
-resource "aws_route_table" "es_private_route_table" {
-  count = 1
-
-  vpc_id = "${var.es_vpc_id}"
-
-  tags {
-    Name = "${var.env_name} | ENT SVCS PRIVATE"
-  }
-}
-
-module "pas_public_vpc_route_table" {
+module "pas_vpc_route_tables" {
   source       = "../vpc_route_table"
   internetless = "${var.internetless}"
   vpc_id       = "${var.pas_vpc_id}"
 
   tags = {
-    Name = "${var.env_name} | PAS PUBLIC"
+    Name = "${var.env_name} | PAS"
   }
 }
 
-module "bastion_public_vpc_route_table" {
+module "bastion_vpc_route_tables" {
   source       = "../vpc_route_table"
   internetless = "${var.internetless}"
   vpc_id       = "${var.bastion_vpc_id}"
 
   tags = {
-    Name = "${var.env_name} | BASTION PUBLIC"
+    Name = "${var.env_name} | BASTION"
   }
 }
 
-module "es_public_vpc_route_table" {
+module "es_vpc_route_tables" {
   source       = "../vpc_route_table"
   internetless = "${var.internetless}"
   vpc_id       = "${var.es_vpc_id}"
 
   tags = {
-    Name = "${var.env_name} | ENT SVCS PUBLIC"
+    Name = "${var.env_name} | ENT SVCS"
   }
 }
 
-module "cp_public_vpc_route_table" {
+module "cp_vpc_route_tables" {
   source       = "../vpc_route_table"
   internetless = "${var.internetless}"
   vpc_id       = "${var.cp_vpc_id}"
 
   tags = {
-    Name = "${var.env_name} | CP PUBLIC"
+    Name = "${var.env_name} | CP"
   }
 }
 
-output "pas_private_vpc_route_table_id" {
-  value = "${element(concat(aws_route_table.pas_private_route_table.*.id, list("")), 0)}"
+output "pas_public_vpc_route_table_id" {
+  value = "${module.pas_vpc_route_tables.public_route_table_id}"
 }
 
-output "pas_public_vpc_route_table_id" {
-  value = "${module.pas_public_vpc_route_table.route_table_id}"
+output "pas_private_vpc_route_table_id" {
+  value = "${module.pas_vpc_route_tables.private_route_table_id}"
 }
 
 output "bastion_public_vpc_route_table_id" {
-  value = "${module.bastion_public_vpc_route_table.route_table_id}"
+  value = "${module.bastion_vpc_route_tables.public_route_table_id}"
 }
 
-output "es_private_vpc_route_table_id" {
-  value = "${element(concat(aws_route_table.es_private_route_table.*.id, list("")), 0)}"
+output "bastion_private_vpc_route_table_id" {
+  value = "${module.bastion_vpc_route_tables.private_route_table_id}"
 }
 
 output "es_public_vpc_route_table_id" {
-  value = "${module.es_public_vpc_route_table.route_table_id}"
+  value = "${module.es_vpc_route_tables.public_route_table_id}"
 }
 
-output "cp_private_vpc_route_table_id" {
-  value = "${element(concat(aws_route_table.cp_private_route_table.*.id, list("")), 0)}"
+output "es_private_vpc_route_table_id" {
+  value = "${module.es_vpc_route_tables.private_route_table_id}"
 }
 
 output "cp_public_vpc_route_table_id" {
-  value = "${module.cp_public_vpc_route_table.route_table_id}"
+  value = "${module.cp_vpc_route_tables.public_route_table_id}"
+}
+
+output "cp_private_vpc_route_table_id" {
+  value = "${module.cp_vpc_route_tables.private_route_table_id}"
 }
