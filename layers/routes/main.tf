fec30af82e1cfe5cc96f146fc9331a7918eaa47c
Fritz B <fbehrens51@gmail.com>
2019-10-02 12:04:35 -0400

    [#168876243] reverting use of env_name tag to filter aws_route_tables as this won't work when they don't exist since they're created within the same outer module.  Refactored to pass through the specific route table ids we create into the submodule which handles the adding of the routes.

--- layers/routes/main.tf
+++ layers/routes/main.tf
@@ -70,42 +70,54 @@ module "route_bastion_cp" {
   source           = "./modules/routing"
   accepter_vpc_id  = "${local.bastion_vpc_id}"
   requester_vpc_id = "${local.cp_vpc_id}"
-  env_name         = "${var.env_name}"
+  accepter_route_table_ids = ["${module.bastion_vpc_route_tables.public_route_table_id}","${module.bastion_vpc_route_tables.private_route_table_id}"]
+  requester_route_table_ids = ["${module.cp_vpc_route_tables.private_route_table_id}","${module.cp_vpc_route_tables.public_route_table_id}"]
+
 }
 
 module "route_bastion_pas" {
   source           = "./modules/routing"
   accepter_vpc_id  = "${local.bastion_vpc_id}"
   requester_vpc_id = "${local.pas_vpc_id}"
-  env_name         = "${var.env_name}"
+  accepter_route_table_ids = ["${module.bastion_vpc_route_tables.public_route_table_id}","${module.bastion_vpc_route_tables.private_route_table_id}"]
+  requester_route_table_ids = ["${module.pas_vpc_route_tables.private_route_table_id}","${module.pas_vpc_route_tables.public_route_table_id}"]
 }
 
 module "route_bastion_es" {
   source           = "./modules/routing"
   accepter_vpc_id  = "${local.bastion_vpc_id}"
   requester_vpc_id = "${local.es_vpc_id}"
-  env_name         = "${var.env_name}"
+  accepter_route_table_ids = ["${module.bastion_vpc_route_tables.public_route_table_id}","${module.bastion_vpc_route_tables.private_route_table_id}"]
+  requester_route_table_ids = ["${module.es_vpc_route_tables.private_route_table_id}","${module.es_vpc_route_tables.public_route_table_id}"]
+
 }
+
 
 module "route_cp_pas" {
   source           = "./modules/routing"
   accepter_vpc_id  = "${local.cp_vpc_id}"
   requester_vpc_id = "${local.pas_vpc_id}"
-  env_name         = "${var.env_name}"
+  accepter_route_table_ids = ["${module.cp_vpc_route_tables.public_route_table_id}","${module.cp_vpc_route_tables.private_route_table_id}"]
+  requester_route_table_ids = ["${module.pas_vpc_route_tables.private_route_table_id}","${module.pas_vpc_route_tables.public_route_table_id}"]
+
 }
 
 module "route_cp_es" {
   source           = "./modules/routing"
   accepter_vpc_id  = "${local.cp_vpc_id}"
   requester_vpc_id = "${local.es_vpc_id}"
-  env_name         = "${var.env_name}"
+  accepter_route_table_ids = ["${module.cp_vpc_route_tables.public_route_table_id}","${module.cp_vpc_route_tables.private_route_table_id}"]
+  requester_route_table_ids = ["${module.es_vpc_route_tables.private_route_table_id}","${module.es_vpc_route_tables.public_route_table_id}"]
+
 }
 
 module "route_pas_es" {
   source           = "./modules/routing"
   accepter_vpc_id  = "${local.pas_vpc_id}"
   requester_vpc_id = "${local.es_vpc_id}"
-  env_name         = "${var.env_name}"
+  accepter_route_table_ids = ["${module.pas_vpc_route_tables.public_route_table_id}","${module.pas_vpc_route_tables.private_route_table_id}"]
+  requester_route_table_ids = ["${module.es_vpc_route_tables.private_route_table_id}","${module.es_vpc_route_tables.public_route_table_id}"]
+
 }
 
 variable "remote_state_region" {}
--- layers/routes/modules/routing/main.tf
+++ layers/routes/modules/routing/main.tf
@@ -1,6 +1,13 @@
 variable "requester_vpc_id" {}
 variable "accepter_vpc_id" {}
-variable "env_name" {}
+
+variable "requester_route_table_ids" {
+  type = "list"
+}
+
+variable "accepter_route_table_ids" {
+  type = "list"
+}
 
 data "aws_vpc" "accepter_vpc" {
   id = "${var.accepter_vpc_id}"
@@ -17,20 +24,17 @@ data "aws_vpc_peering_connection" "peering_connection" {
 
 data "aws_route_tables" "accepter_route_table" {
   vpc_id = "${var.accepter_vpc_id}"
-
   filter {
-    name   = "tag:Name"
-    values = ["${var.env_name}*"]
+    name = "route-table-id"
+    values = ["${var.accepter_route_table_ids}"]
   }
-
 }
 
 data "aws_route_tables" "requester_route_table" {
   vpc_id = "${var.requester_vpc_id}"
-
   filter {
-    name   = "tag:Name"
-    values = ["${var.env_name}*"]
+    name = "route-table-id"
+    values = ["${var.requester_route_table_ids}"]
   }
 }
 
