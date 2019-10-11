20c2db8ece51a13335ab58784452881a233b54b9
Patrick Oyarzun <poyarzun@pivotal.io>
2019-10-11 21:51:47 +0000

    Turn on TLS for syslog traffic from clamav and pas
    [#168782719](https://www.pivotaltracker.com/story/show/168782719)
    
    Co-authored-by: Patrick Oyarzun <poyarzun@pivotal.io>
    Co-authored-by: John Shahid <jshahid@pivotal.io>

--- modules/ops_manager_config/main.tf
+++ modules/ops_manager_config/main.tf
@@ -199,8 +199,9 @@ data "template_file" "cf_template" {
     pas_vpc_azs                 = "${indent(4, join("", data.template_file.pas_vpc_azs.*.rendered))}"
     singleton_availability_zone = "${var.singleton_availability_zone}"
 
-    splunk_syslog_host = "${var.splunk_syslog_host}"
-    splunk_syslog_port = "${var.splunk_syslog_port}"
+    splunk_syslog_host    = "${var.splunk_syslog_host}"
+    splunk_syslog_port    = "${var.splunk_syslog_port}"
+    splunk_syslog_ca_cert = "${var.splunk_syslog_ca_cert}"
 
     backup_restore_instance_type                = "${var.backup_restore_instance_type}"
     clock_global_instance_type                  = "${var.clock_global_instance_type}"
