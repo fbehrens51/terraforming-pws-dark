7bf8348a892f0207844920833b2cd8a944b661ab
Patrick Oyarzun <poyarzun@pivotal.io>
2019-10-11 17:43:25 +0000

    Delete a comment
    This is not needed anymore, we are setting the owners array later in the file.
    
    Co-authored-by: Patrick Oyarzun <poyarzun@pivotal.io>
    Co-authored-by: John Shahid <jshahid@pivotal.io>

--- modules/amis/encrypted/amazon2/lookup/main.tf
+++ modules/amis/encrypted/amazon2/lookup/main.tf
@@ -3,14 +3,6 @@ data "aws_caller_identity" "current" {}
 data "aws_ami" "current_ami" {
   most_recent = true
 
-//  filter {
-//    name = "owner-alias"
-//
-//    values = [
-//      "${data.aws_caller_identity.current.account_id}",
-//    ]
-//  }
-
   name_regex = "^encrypted_amzn2-ami-hvm-[0-9.]+-x86_64-ebs-[0-9]*$"
 
   owners = ["${data.aws_caller_identity.current.account_id}"]
