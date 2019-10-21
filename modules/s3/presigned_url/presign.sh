1cd470c6f935a939371b0987a0ad62b21ac89fa6
Patrick Oyarzun <poyarzun@pivotal.io>
2019-10-21 14:55:31 +0000

    Stop using pre-signed URLs when launching ops manager
    Instead download the debian packages from public bucket.  Also got rid of the
    presigned_url and the launch_ignore_user_data modules.
    
    Changed both ops-manager layers to use the full filename instead of a prefix
    for two reasons:
    
    1. We cannot look up the latest version in terraform without using the cli
    2. This aligns us better with using a manifest to list the versions of all
    artifacts used in the deployment.
    
    [#168782145](https://www.pivotaltracker.com/story/show/168782145)
    
    Co-authored-by: Patrick Oyarzun <poyarzun@pivotal.io>
    Co-authored-by: John Shahid <jshahid@pivotal.io>

--- modules/s3/presigned_url/presign.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-eval "$(jq -r '@sh "BUCKET_NAME=\(.bucket_name) OBJECT_PREFIX=\(.object_prefix)"')"
-object_key=$(aws s3api list-objects-v2 --bucket $BUCKET_NAME --prefix $OBJECT_PREFIX --query "reverse(sort_by(Contents, &LastModified))[0].Key"|jq . -r)
-presigned_url=$(aws s3 presign s3://$BUCKET_NAME/$object_key)
-jq -n --arg url "$presigned_url" '{"url": $url}'
