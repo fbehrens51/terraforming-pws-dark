cd307dfb8341901406dc386e7deea4b88b97680a
Fritz B <fbehrens51@gmail.com>
2019-11-18 18:28:58 -0500

    [#169167800] replaced custom logic to create encrypted volume with use of aws_ami_copy resource

--- modules/amis/encrypted/amazon2/create/main.tf
+++ modules/amis/encrypted/amazon2/create/main.tf
@@ -16,20 +16,18 @@ data "aws_ami" "current_ami" {
   owners = []
 }
 
-data "aws_availability_zones" "azs" {
-  state = "available"
+data "aws_region" "current_region" {
+  current = true
 }
 
-resource "aws_ebs_volume" "encrypted_volume" {
-  availability_zone = "${data.aws_availability_zones.azs.names[0]}"
-  encrypted         = true
-  kms_key_id        = "${var.kms_key_id}"
-  snapshot_id       = "${data.aws_ami.current_ami.root_snapshot_id}"
+resource "random_integer" "name_extension" {
+  max = 999999999
+  min = 0
 }
-
-module "create_ami" {
-  source      = "../../create_ami_from_volume"
-  volume_id   = "${aws_ebs_volume.encrypted_volume.id}"
-  name_prefix = "encrypted_${data.aws_ami.current_ami.name}"
-  depends_on  = ["${aws_ebs_volume.encrypted_volume.arn}"]
+resource "aws_ami_copy" "encrypted_amazon2_ami" {
+  name = "encrypted_${data.aws_ami.current_ami.name}-${random_integer.name_extension.result}"
+  source_ami_id = "${data.aws_ami.current_ami.id}"
+  source_ami_region = "${data.aws_region.current_region.name}"
+  encrypted = true
+  kms_key_id = "${var.kms_key_id}"
 }
