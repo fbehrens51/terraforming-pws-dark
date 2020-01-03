411f7ef1346e800ba3ede9e76ed805475c8b2a5f
Fritz B <fbehrens51@gmail.com>
2020-01-03 09:29:50 -0500

    [#170402312] As part of upgrading healthwatch to 1.8, we increment it to 1.7.1 first

--- modules/ops_manager_config/main.tf
+++ modules/ops_manager_config/main.tf
@@ -12,7 +12,7 @@ locals {
 
   healthwatch_file_glob       = "p-healthwatch*.pivotal"
   healthwatch_product_slug    = "p-healthwatch"
-  healthwatch_product_version = "1.7.0"
+  healthwatch_product_version = "1.7.1"
 
   compliance_scanner_file_glob       = "p-compliance-scanner*.pivotal"
   compliance_scanner_product_slug    = "p-compliance-scanner"
