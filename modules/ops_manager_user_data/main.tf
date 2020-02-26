662318be6ce63206fd4c0f2a3db345fda4331c66
John Shahid <jshahid@pivotal.io>
2020-02-26 20:16:44 +0000

    Split the CA certs to multiple files similar to the way BOSH does it.
    See code comments for more details.
    
    [#171339847](https://www.pivotaltracker.com/story/show/171339847)
    
    Authored-by: John Shahid <jshahid@pivotal.io>

--- modules/ops_manager_user_data/main.tf
+++ modules/ops_manager_user_data/main.tf
@@ -30,31 +30,71 @@ data "template_cloudinit_config" "config" {
   }
 
   part {
-    filename     = "certs.cfg"
-    content_type = "text/cloud-config"
-    content      = <<CLOUDINIT
-ca-certs:
-  # remove all the default trusted CA certificates that are normally shipped
-  # with Ubuntu.
-  remove-defaults: true
-
-  # The following are the CAs used in the AWS EC2 and S3 endpoint cert chain.
-  trusted:
-  - |
-    ${indent(4, var.trusted_ca_certs)}
-CLOUDINIT
-
-  }
-
-  part {
     content_type = "text/cloud-config"
     merge_type   = "list(append)+dict(no_replace,recurse_list)"
     content      = <<CLOUDINIT
+
+ca-certs:
+  remove-defaults: true
+
 bootcmd:
   # Disable SSL in postgres.  Otherwise, postgres will fail to start since the
   # snakeoil certificate is missing.  Note that OM connect to postgres over the
   # unix socket.
   - sudo sed -i 's/^ssl = true/#ssl = true/' /etc/postgresql/*/main/postgresql.conf
+
+# We cannot use cloud-init `ca-certs` to set the trusted CAs. cloud-init will
+# put all the certs in a single file,
+# i.e. /etc/ssl/certs/cloud-init-ca-certs.pem. This will cause /etc/ssl/certs
+# to have a single symlink, e.g.:
+#
+# 02db8165.0 -> cloud-init-ca-certs.crt
+#
+# As opposed to the having multiple symlinks, such as:
+#
+# bef70e06.0 -> cloud_init_ca_cert_1.pem
+# 09789157.0 -> cloud_init_ca_cert_2.pem
+# 653b494a.0 -> cloud_init_ca_cert_3.pem
+#
+# Many libraries do certificates lookup using the cert hash. In particular, I
+# believe the AWS CPI breaks if not all CA certificates have a symlink.
+
+
+write_files:
+- path: /run/certs.crt
+  content: |
+    ${indent(4, var.trusted_ca_certs)}
+
+runcmd:
+  - |
+    set -ex
+
+    echo "deleting old certificates"
+    rm -f /usr/local/share/ca-certificates/cloud_init_ca_cert_*
+
+    # Split files on '----END CERTIFICATE-----' and increment our file counter by 1
+    awk -v n=1 '
+      split_after == 1 {n++;split_after=0}
+      /-----END CERTIFICATE-----/ {split_after=1}
+      NF {print > "/usr/local/share/ca-certificates/cloud_init_ca_cert_" n ".crt"}' < /run/certs.crt
+
+    updated_certs=1
+    retry_count=0
+    max_retry_count=3
+
+    set +e
+    until [ $updated_certs -eq 0 ] || [ $retry_count -ge $max_retry_count ]; do
+      echo "trying to run update-ca-certificates..."
+      timeout --signal=KILL 60s /usr/sbin/update-ca-certificates -f -v
+      updated_certs=$?
+      retry_count=$((retry_count + 1))
+    done
+    set -e
+
+    if [ $updated_certs -ne 0 ]; then
+      echo "failed to setup ca certificates"
+      exit 1
+    fi
 CLOUDINIT
   }
 
