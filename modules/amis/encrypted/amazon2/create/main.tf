de7840622b38d0d3c18cc178fe66cee1bef81f77
John Shahid <jshahid@pivotal.io>
2020-03-02 22:39:35 +0000

    Reuse the modules/amis/amazon_hvm_ami to do the AMI lookup
    We shouldn't have the AMI regexp in multiple places.
    
    Authored-by: John Shahid <jshahid@pivotal.io>

--- modules/amis/encrypted/amazon2/create/main.tf
+++ modules/amis/encrypted/amazon2/create/main.tf
@@ -1,20 +1,16 @@
 variable "kms_key_id" {
 }
 
-data "aws_ami" "current_ami" {
-  most_recent = true
-
-  name_regex = "^amzn2-ami-hvm-[0-9.]+-x86_64-ebs$"
-
-  owners = ["amazon"]
-}
-
 data "aws_region" "current_region" {
 }
 
+module "amazon2_linux_ami" {
+  source = "../../../amazon_hvm_ami"
+}
+
 resource "aws_ami_copy" "encrypted_amazon2_ami" {
-  name              = "encrypted_${data.aws_ami.current_ami.name}"
-  source_ami_id     = data.aws_ami.current_ami.id
+  name              = "encrypted_${module.amazon2_linux_ami.name}"
+  source_ami_id     = module.amazon2_linux_ami.id
   source_ami_region = data.aws_region.current_region.name
   encrypted         = true
   kms_key_id        = var.kms_key_id
