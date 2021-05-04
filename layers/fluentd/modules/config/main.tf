4b16df42c30b80cb3fa4474924141a4f889d7d3c
Bryan Neva <bneva@pivotal.io>
2021-05-04 15:46:12 -0400

    Restore fluentd bucket key

--- layers/fluentd/modules/config/main.tf
+++ layers/fluentd/modules/config/main.tf
@@ -21,9 +21,7 @@ variable "server_cert" {
 }
 
 locals {
-//  Temporarily revert bucket_key to debug pipeline
-//  bucket_key = "fluentd-${md5(data.template_file.certs_user_data.rendered)}-user-data.yml"
-  bucket_key = "fluentd-user-data.yml"
+  bucket_key = "fluentd-${md5(data.template_file.certs_user_data.rendered)}-user-data.yml"
 }
 
 module "ports" {
