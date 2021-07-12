8a025a02c2b7727c7b3e15bc2a83e6cd21131fd3
Bryan Neva <bneva@vmware.com>
2021-07-12 11:15:00 -0400

    [#175129690] Update split iam to retain existing roles while adding bootstrap and foundation

--- layers/ops-manager/main.tf
+++ layers/ops-manager/main.tf
@@ -53,6 +53,10 @@ locals {
   super_user_id         = data.terraform_remote_state.paperwork.outputs.super_user_ids
   om_eni_id             = data.terraform_remote_state.pas.outputs.om_eni_id
 
+  bootstrap_role_id  = data.terraform_remote_state.paperwork.outputs.bootstrap_role_id
+  foundation_role_id = data.terraform_remote_state.paperwork.outputs.foundation_role_id
+  foundation_role_name = data.terraform_remote_state.paperwork.outputs.foundation_role_name
+
   env_name      = var.global_vars.env_name
   modified_name = "${var.global_vars.name_prefix} ops-manager"
   modified_tags = merge(
@@ -102,7 +106,7 @@ module "ops_manager" {
 
   source               = "../../modules/launch"
   ami_id               = var.om_ami_id
-  iam_instance_profile = local.om_role_name
+  iam_instance_profile = local.foundation_role_name
   instance_types       = data.terraform_remote_state.scaling-params.outputs.instance_types
   scale_vpc_key        = "pas"
   scale_service_key    = "ops-manager"
@@ -122,7 +126,8 @@ module "ops_manager_backup_bucket_policy" {
   source     = "../../modules/bucket/policy/generic"
   bucket_arn = data.terraform_remote_state.pas.outputs.om_bucket_arn
 
-  read_write_role_ids = concat(local.super_user_role_ids, [local.director_role_id, local.om_role_id])
+  read_write_role_ids = concat(local.super_user_role_ids, [local.director_role_id, local.om_role_id,
+    local.bootstrap_role_id, local.foundation_role_id])
   read_write_user_ids = local.super_user_role_ids
   read_only_role_ids  = [local.isse_role_id, local.ent_tech_read_role_id]
   disable_delete      = false
