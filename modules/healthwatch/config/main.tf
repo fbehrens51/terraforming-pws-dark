6bab748be4175bca6ec1b7bce7bf6e1a505030af
Patrick Oyarzun <poyarzun@vmware.com>
2021-07-28 12:03:38 -0400

    Add automatic configuration of healthwatch for control plane metrics
    - If the necessary certs exist in the secrets bucket, healthwatch will be
    configured to read metrics from the control plane BOSH VMs
    
    [#177162939]
    
    Authored-by: Patrick Oyarzun <poyarzun@vmware.com>

--- modules/healthwatch/config/main.tf
+++ modules/healthwatch/config/main.tf
@@ -11,6 +11,14 @@ variable "grafana_server_key" {}
 variable "network_name" {
 }
 
+variable "control_plane_vpc_id" {}
+variable "control_plane_metrics_certificate" {}
+variable "control_plane_metrics_ca_certificate" {}
+variable "control_plane_metrics_private_key" {}
+variable "control_plane_metrics_enabled" {
+  type = bool
+}
+
 variable "availability_zones" {
   type = list(string)
 }
@@ -108,36 +116,41 @@ module "domains" {
 
 locals {
   healthwatch_config = templatefile("${path.module}/healthwatch_config.tpl", {
-    scale                            = var.scale["p-healthwatch2"]
-    env_tag_name                     = var.env_tag_name
-    grafana_additional_cipher_suites = join(",", var.grafana_additional_cipher_suites)
-    grafana_root_url                 = "https://${module.domains.grafana_fqdn}"
-    fluentd_root_url                 = "${module.domains.fluentd_fqdn}:9200"
-    canary_url                       = "https://${module.domains.apps_manager_fqdn}"
-    ops_canary_url                   = "https://${module.domains.om_fqdn}/api/v0/info"
-    metrics_key                      = var.metrics_key
-    root_ca_cert                     = var.grafana_server_ca_cert
-    grafana_server_cert              = var.grafana_server_cert
-    grafana_server_key               = var.grafana_server_key
-    network_name                     = var.network_name
-    hw_vpc_azs                       = indent(2, join("", data.template_file.hw_vpc_azs.*.rendered))
-    singleton_availability_zone      = var.singleton_availability_zone
-    region                           = var.region
-    grafana_elb_id                   = var.grafana_elb_id
-    grafana_uaa_client_secret        = var.grafana_uaa_client_secret
-    uaa_url                          = "https://uaa.${module.domains.system_fqdn}"
-    smtp_enabled                     = var.smtp_from == "" ? false : true
-    smtp_from                        = var.smtp_from
-    smtp_host                        = var.smtp_host
-    smtp_password                    = var.smtp_client_password
-    smtp_port                        = var.smtp_client_port
-    smtp_user                        = var.smtp_client_user
-    email_addresses                  = var.email_addresses
-    slack_enabled                    = var.slack_webhook == "" ? false : true
-    slack_webhook                    = var.slack_webhook
-    syslog_host                      = var.syslog_host
-    syslog_port                      = var.syslog_port
-    syslog_ca_cert                   = var.syslog_ca_cert
+    scale                                = var.scale["p-healthwatch2"]
+    env_tag_name                         = var.env_tag_name
+    grafana_additional_cipher_suites     = join(",", var.grafana_additional_cipher_suites)
+    grafana_root_url                     = "https://${module.domains.grafana_fqdn}"
+    fluentd_root_url                     = "${module.domains.fluentd_fqdn}:9200"
+    canary_url                           = "https://${module.domains.apps_manager_fqdn}"
+    ops_canary_url                       = "https://${module.domains.om_fqdn}/api/v0/info"
+    metrics_key                          = var.metrics_key
+    root_ca_cert                         = var.grafana_server_ca_cert
+    grafana_server_cert                  = var.grafana_server_cert
+    grafana_server_key                   = var.grafana_server_key
+    network_name                         = var.network_name
+    hw_vpc_azs                           = indent(2, join("", data.template_file.hw_vpc_azs.*.rendered))
+    singleton_availability_zone          = var.singleton_availability_zone
+    region                               = var.region
+    grafana_elb_id                       = var.grafana_elb_id
+    grafana_uaa_client_secret            = var.grafana_uaa_client_secret
+    uaa_url                              = "https://uaa.${module.domains.system_fqdn}"
+    smtp_enabled                         = var.smtp_from == "" ? false : true
+    smtp_from                            = var.smtp_from
+    smtp_host                            = var.smtp_host
+    smtp_password                        = var.smtp_client_password
+    smtp_port                            = var.smtp_client_port
+    smtp_user                            = var.smtp_client_user
+    email_addresses                      = var.email_addresses
+    slack_enabled                        = var.slack_webhook == "" ? false : true
+    slack_webhook                        = var.slack_webhook
+    syslog_host                          = var.syslog_host
+    syslog_port                          = var.syslog_port
+    syslog_ca_cert                       = var.syslog_ca_cert
+    control_plane_vpc_id                 = var.control_plane_vpc_id
+    control_plane_metrics_ca_certificate = var.control_plane_metrics_ca_certificate
+    control_plane_metrics_certificate    = var.control_plane_metrics_certificate
+    control_plane_metrics_private_key    = var.control_plane_metrics_private_key
+    control_plane_metrics_enabled        = var.control_plane_metrics_enabled
 
     alerting_rules_yml = templatefile("${path.module}/alerting_rules.yml", {
       log_group_name = var.log_group_name
