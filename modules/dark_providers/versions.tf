4a9daab49c41a318e26db0d0f9b7bcc8caacf7b2
Mark Houliston <24305231+Houlistonm@users.noreply.github.com>
2021-08-23 08:55:42 -0400

    [#179224940] update the versions.tf

--- modules/dark_providers/versions.tf
+++ modules/dark_providers/versions.tf
@@ -1,32 +1,49 @@
 terraform {
+  required_version = ">= 1.0.5"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-    }
-    dns = {
-      source = "hashicorp/dns"
-    }
-    external = {
-      source = "hashicorp/external"
-    }
-    grafana = {
-      source = "grafana/grafana"
-    }
-    local = {
-      source = "hashicorp/local"
-    }
-    null = {
-      source = "hashicorp/null"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    template = {
-      source = "hashicorp/template"
+      source  = "hashicorp/aws"
+      version = "2.49.0"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
+      version = "~> 2.1.1"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 2.1.0"
+    }
+
+    template = {
+      source  = "hashicorp/template"
+      version = "~> 2.1.2"
+    }
+
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 2.1.2"
+    }
+
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 1.1.2"
+    }
+
+    dns = {
+      source  = "hashicorp/dns"
+      version = "~> 2.1.1"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 1.4.0"
+    }
+
+    grafana = {
+      source  = "grafana/grafana"
+      version = "~> 1.13.4"
     }
   }
-  required_version = ">= 1.0.5"
 }
