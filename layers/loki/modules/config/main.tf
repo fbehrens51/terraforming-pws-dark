c6739206a2467eb2b63f3021f9171d7c71a256fa
Patrick Oyarzun <poyarzun@vmware.com>
2021-09-07 18:00:17 -0400

    Use templatefile
    Authored-by: Patrick Oyarzun <poyarzun@vmware.com>

--- layers/loki/modules/config/main.tf
+++ layers/loki/modules/config/main.tf
@@ -3,18 +3,6 @@ module "ports" {
   source = "../../../../modules/syslog_ports"
 }
 
-data "template_file" "loki_configuration" {
-  template = file("${path.module}/loki.yaml")
-
-  vars = {
-    http_port      = module.ports.loki_http_port
-    grpc_port      = module.ports.loki_grpc_port
-    region         = var.region
-    loki_ips       = var.loki_ips
-    storage_bucket = var.storage_bucket
-  }
-}
-
 data "template_file" "config_user_data" {
   template = file("${path.module}/user_data.tpl")
 
@@ -30,6 +18,13 @@ data "template_file" "config_user_data" {
 locals {
   bucket_key    = "loki-${md5(data.template_file.config_user_data.rendered)}-user-data.yml"
   loki_location = "${var.public_bucket_url}/${var.loki_bundle_key}"
+  loki_configuration = templatefile("${path.module}/loki.yaml", {
+    http_port      = module.ports.loki_http_port
+    grpc_port      = module.ports.loki_grpc_port
+    region         = var.region
+    loki_ips       = var.loki_ips
+    storage_bucket = var.storage_bucket
+  })
 }
 
 resource "aws_s3_bucket_object" "config_user_data" {
