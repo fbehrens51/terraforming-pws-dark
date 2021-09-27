b3cd35ecfb0e2de8b01ed9660b09640133cf6b3a
Patrick Oyarzun <poyarzun@vmware.com>
2021-09-27 11:06:56 -0400

    Migrate to templatefile function
    Authored-by: Patrick Oyarzun <poyarzun@vmware.com>

--- layers/fluentd/modules/config/main.tf
+++ layers/fluentd/modules/config/main.tf
@@ -1,16 +1,8 @@
 
 locals {
   bucket_key = "fluentd-${md5(data.template_file.certs_user_data.rendered)}-user-data.yml"
-}
 
-module "ports" {
-  source = "../../../../modules/syslog_ports"
-}
-
-data "template_file" "td_agent_configuration" {
-  template = file("${path.module}/td-agent.tpl")
-
-  vars = {
+  td_agent_configuration = templatefile("${path.module}/td-agent.tpl", {
     syslog_port                     = module.ports.syslog_port
     s3_logs_bucket                  = var.s3_logs_bucket
     region                          = var.region
@@ -21,30 +13,30 @@ data "template_file" "td_agent_configuration" {
     s3_path                         = var.s3_path
 
     loki_config = var.loki_config
-  }
+  })
+
+  certs_user_data = templatefile("${path.module}/certs.tpl", {
+    ca_cert     = var.ca_cert
+    server_key  = var.server_key
+    server_cert = var.server_cert
+
+    loki_config = var.loki_config
+  })
+}
+
+module "ports" {
+  source = "../../../../modules/syslog_ports"
 }
 
 data "template_file" "config_user_data" {
   template = file("${path.module}/user_data.tpl")
 
   vars = {
-    td_agent_configuration = data.template_file.td_agent_configuration.rendered
+    td_agent_configuration = local.td_agent_configuration
 
     region             = var.region
     public_bucket_name = var.public_bucket_name
     fluentd_bundle_key = var.fluentd_bundle_key
-  }
-}
-
-data "template_file" "certs_user_data" {
-  template = file("${path.module}/certs.tpl")
-
-  vars = {
-    ca_cert     = var.ca_cert
-    server_key  = var.server_key
-    server_cert = var.server_cert
-
-    loki_config = var.loki_config
   }
 }
 
