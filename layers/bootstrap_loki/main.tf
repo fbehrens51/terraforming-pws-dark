1aafcbcd15bff3c24e85690defa7ae7fb09c7060
Mark Houliston <24305231+Houlistonm@users.noreply.github.com>
2022-02-28 08:33:31 -0500

    [#181270108] locked down loki healthcheck ingress rules for network load balancer

--- layers/bootstrap_loki/main.tf
+++ layers/bootstrap_loki/main.tf
@@ -63,12 +63,11 @@ locals {
       cidr_blocks = "0.0.0.0/0"
     },
     {
-      # TODO: this should be open only internally
       description = "Allow http/${module.syslog_ports.loki_healthcheck_port} (healthcheck) from the load balancer"
       port        = module.syslog_ports.loki_healthcheck_port
       protocol    = "tcp"
-      cidr_blocks = "0.0.0.0/0"
-      #cidr_blocks = join(",", data.aws_subnet.private_subnets.*.cidr_block)
+      cidr_blocks = join(",", data.aws_subnet.public_subnets.*.cidr_block)
+
     },
     {
       // node_exporter metrics endpoint for grafana
