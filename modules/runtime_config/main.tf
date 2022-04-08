3bd4d307c6107a8fd2ac79db5e8bdada5d65ebad
fbehrens51 <fbehrens@vmware.com>
2022-04-08 10:11:16 -0400

    [#180235504] remove IPSec configuration from runtime_config

--- modules/runtime_config/main.tf
+++ modules/runtime_config/main.tf
@@ -1,14 +1,3 @@
-variable "ipsec_log_level" {
-}
-
-variable "ipsec_subnet_cidrs" {
-  type = list(string)
-}
-
-variable "no_ipsec_subnet_cidrs" {
-  type = list(string)
-}
-
 variable "custom_ssh_banner" {
   type        = string
   description = "Custom SSH Banner to be used on launched VMs"
@@ -20,16 +9,12 @@ variable "s3_endpoint" {
 variable "region" {
 }
 
-variable "vpc_dns" {
-}
-
 variable "runtime_config" {
 }
 
 locals {
   runtime_config_file_glob    = "pws-dark-runtime-config*.pivotal"
   runtime_config_product_slug = "pws-dark-runtime-config-tile"
-  vpc_dns_subnet              = "${var.vpc_dns}/32"
 }
 
 resource "aws_s3_bucket_object" "runtime_config_template" {
@@ -37,12 +22,6 @@ resource "aws_s3_bucket_object" "runtime_config_template" {
   key    = var.runtime_config
   content = templatefile("${path.module}/runtime_config_template.tpl", {
     runtime_config     = var.runtime_config
-    ipsec_log_level    = var.ipsec_log_level
-    ipsec_subnet_cidrs = join(",", var.ipsec_subnet_cidrs)
-    no_ipsec_subnet_cidrs = join(
-      ",",
-      concat(var.no_ipsec_subnet_cidrs, [local.vpc_dns_subnet]),
-    )
     ssh_banner  = var.custom_ssh_banner
     extra_users = var.extra_users
   })
