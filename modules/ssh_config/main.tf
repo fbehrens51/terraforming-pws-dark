a7f861ebcc740597dfa298731b61b873bc08a456
fbehrens51 <fbehrens@vmware.com>
2022-07-11 17:19:56 -0400

    [#179241984] formatting and moving files to custom prefix path instead of custom postfix on each file name

--- modules/ssh_config/main.tf
+++ modules/ssh_config/main.tf
@@ -1,44 +1,44 @@
 locals {
 
   bastion_name = "${var.foundation_name}_bastion"
-  sjb_name = "${var.foundation_name}_sjb"
+  sjb_name     = "${var.foundation_name}_sjb"
 
   common_params = {
-    foundation_name = var.foundation_name,
-    ssh_user = var.ssh_user,
-    bastion_name = local.bastion_name
-    sjb_name = local.sjb_name
-    ssh_host_ips    = var.host_ips
-    ssh_host_prefix = var.host_type
-    ssh_key_path    = var.ssh_key_path,
-    custom_ssh_key  = var.custom_ssh_key,
+    foundation_name     = var.foundation_name,
+    ssh_user            = var.ssh_user,
+    bastion_name        = local.bastion_name
+    sjb_name            = local.sjb_name
+    ssh_host_ips        = var.host_ips
+    ssh_host_prefix     = var.host_type
+    ssh_key_path        = var.ssh_key_path,
+    custom_ssh_key      = var.custom_ssh_key,
     include_base_config = var.include_base_config
   }
 
-  is_sjb = (var.host_type=="sjb" ? true : false)
-  is_bastion = (var.host_type=="bastion" ? true : false)
-  is_base = (var.host_type=="base" ? true : false)
+  is_sjb     = (var.host_type == "sjb" ? true : false)
+  is_bastion = (var.host_type == "bastion" ? true : false)
+  is_base    = (var.host_type == "base" ? true : false)
 
   sshconfig_outside = templatefile("${path.module}/sshconfig.tpl",
     merge(local.common_params,
-          {
-            proxy_jump = (local.is_sjb ? local.bastion_name : var.custom_inner_proxy),
-            enable_sjb_proxyjump = (local.is_base ? true : false),
-          }
+      {
+        proxy_jump           = (local.is_sjb ? local.bastion_name : var.custom_inner_proxy),
+        enable_sjb_proxyjump = (local.is_base ? true : false),
+      }
     )
   )
   sshconfig_bastion = templatefile("${path.module}/sshconfig.tpl",
     merge(local.common_params,
-        {
-          proxy_jump = var.custom_inner_proxy,
-          enable_sjb_proxyjump = (local.is_base ? true : false),
-        }
+      {
+        proxy_jump           = var.custom_inner_proxy,
+        enable_sjb_proxyjump = (local.is_base ? true : false),
+      }
     )
   )
   sshconfig_sjb = templatefile("${path.module}/sshconfig.tpl",
     merge(local.common_params,
       {
-        proxy_jump = var.custom_inner_proxy,
+        proxy_jump           = var.custom_inner_proxy,
         enable_sjb_proxyjump = false,
       }
     )
@@ -46,20 +46,20 @@ locals {
 }
 
 variable "custom_inner_proxy" {
-  type = string
-  default = ""
+  type        = string
+  default     = ""
   description = "custom inner_proxy"
 }
 
 variable "include_base_config" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Whether or not to include the base sshconfig settings in the generated config"
 }
 
 variable "enable_sjb_proxyjump" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "Whether or not to include the custom sjb proxyjump config"
 }
 
@@ -70,7 +70,7 @@ variable "foundation_name" {
 
 variable "ssh_user" {
   type        = string
-  default = ""
+  default     = ""
   description = "user used for the provided host's sshconfig"
 }
 
@@ -81,7 +81,7 @@ variable "host_type" {
 
 variable "host_ips" {
   description = "list of hostname, IP address(es)"
-  default = []
+  default     = []
 }
 
 variable "custom_ssh_key" {
@@ -96,7 +96,7 @@ variable "ssh_key_path" {
 }
 
 variable "secrets_bucket_name" {
-  type    = string
+  type        = string
   description = "name of the bucket to write config to"
 }
 
@@ -114,23 +114,23 @@ output "ssh_config_sjb" {
 
 resource "aws_s3_bucket_object" "sshconfig_outside" {
   bucket       = var.secrets_bucket_name
-  key          = "sshconfig_partial/${var.foundation_name}_${var.host_type}_sshconfig_outside"
+  key          = "sshconfig/outside/${var.foundation_name}_${var.host_type}_sshconfig"
   content      = local.sshconfig_outside
   content_type = "text/plain"
 }
 
 resource "aws_s3_bucket_object" "sshconfig_bastion" {
-  count       = (local.is_bastion ? 0 : 1)
+  count        = (local.is_bastion ? 0 : 1)
   bucket       = var.secrets_bucket_name
-  key          = "sshconfig_partial/${var.foundation_name}_${var.host_type}_sshconfig_bastion"
+  key          = "sshconfig/bastion/${var.foundation_name}_${var.host_type}_sshconfig"
   content      = local.sshconfig_bastion
   content_type = "text/plain"
 }
 
 resource "aws_s3_bucket_object" "sshconfig_sjb" {
-  count       = (local.is_sjb ? 0 : (local.is_bastion ? 0 : 1))
+  count        = (local.is_sjb ? 0 : (local.is_bastion ? 0 : 1))
   bucket       = var.secrets_bucket_name
-  key          = "sshconfig_partial/${var.foundation_name}_${var.host_type}_sshconfig_sjb"
+  key          = "sshconfig/sjb/${var.foundation_name}_${var.host_type}_sshconfig"
   content      = local.sshconfig_sjb
   content_type = "text/plain"
 }
