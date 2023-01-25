748b878acb43f0e4e151beb3e8d84839f33974be
fbehrens51 <fbehrens@vmware.com>
2023-01-25 09:36:40 -0500

    [#184314131] Unable to ssh to *_pas\_nat\_* vms

--- modules/infra/nat.tf
+++ modules/infra/nat.tf
@@ -1,30 +1,3 @@
-#module "nat" {
-#  source                     = "../nat"
-#  ami_id                     = var.nat_ami_id
-#  private_route_table_ids    = var.private_route_table_ids
-#  ingress_cidr_blocks        = [data.aws_vpc.vpc.cidr_block]
-#  metrics_ingress_cidr_block = data.aws_vpc.vpc.cidr_block
-#  tags                       = var.tags
-#  public_subnet_ids          = aws_subnet.public_subnets.*.id
-#  ssh_cidr_blocks            = var.ssh_cidr_blocks
-#  internetless               = var.internetless
-#  instance_types             = var.instance_types
-#  scale_vpc_key              = "pas"
-#  user_data                  = var.user_data
-#  bot_key_pem                = var.bot_key_pem
-#  check_cloud_init           = var.check_cloud_init
-#  operating_system           = var.operating_system
-#
-#  root_domain    = var.root_domain
-#  syslog_ca_cert = var.syslog_ca_cert
-#
-#  public_bucket_name = var.public_bucket_name
-#  public_bucket_url  = var.public_bucket_url
-#  role_name          = var.default_instance_role_name
-#}
-
-
-
 locals {
 
   modified_name = "${var.tags.tags["Name"]}"
@@ -51,7 +24,7 @@ module "nat" {
   metrics_ingress_cidr_block = data.aws_vpc.vpc.cidr_block
   tags                       = { tags = local.modified_tags, instance_tags = local.instance_tags }
   public_subnet_ids          = aws_subnet.public_subnets.*.id
-  ssh_cidr_blocks            = [data.aws_vpc.vpc.cidr_block]
+  ssh_cidr_blocks            = [var.ssh_cidr_blocks]
   internetless               = var.internetless
   instance_types             = var.instance_types
   scale_vpc_key              = "pas"
